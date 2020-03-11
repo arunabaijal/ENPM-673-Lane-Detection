d9a30902d5fc792ba95d4be04280a74804aafd5a
Aruna Baijal <aruna11baijal@gmail.com>
2020-03-11 13:05:18 -0400

    Merging part1 and part3

--- homography.py
+++ homography.py
@@ -3,6 +3,7 @@ import cv2
 from copy import deepcopy
 import matplotlib.pyplot as plt
 import os
+import math
 
 def threshIt(img, thresh_min=10, thresh_max=160):
     """
@@ -29,48 +30,48 @@ K = np.asarray([[9.037596e+02, 0.000000e+00, 6.957519e+02],
 D = np.asarray([-3.639558e-01, 1.788651e-01, 6.029694e-04, -3.922424e-04, -5.382460e-02])
 
 if select_data == 0:
-	cap = cv2.VideoCapture('data_2/challenge_video.mp4')
-	if (cap.isOpened() == False):
-	    print("Unable to read camera feed")
-	# out = cv2.VideoWriter('tag1CubeResult.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, (frame_width, frame_height))
-	while (True):
-	    ret, im = cap.read()
-	    if ret == True:
-	        im = cv2.undistort(im, K, D) 
-	        img_yuv = cv2.cvtColor(im, cv2.COLOR_BGR2YUV)
-	        img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
-	        im = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
-	        im = cv2.GaussianBlur(im, (5,5), 5)
-	        images.append(im)
-	    else:
-	        cap.release()
-	        break
+    cap = cv2.VideoCapture('data_2/challenge_video.mp4')
+    if (cap.isOpened() == False):
+        print("Unable to read camera feed")
+    # out = cv2.VideoWriter('tag1CubeResult.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, (frame_width, frame_height))
+    while (True):
+        ret, im = cap.read()
+        if ret == True:
+            im = cv2.undistort(im, K, D)
+            img_yuv = cv2.cvtColor(im, cv2.COLOR_BGR2YUV)
+            img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
+            im = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+            im = cv2.GaussianBlur(im, (5,5), 5)
+            images.append(im)
+        else:
+            cap.release()
+            break
 
-	src1 = np.array([[(280, 700), (1100, 700), (760, 480), (600, 480)]])
+    src1 = np.array([[(280, 700), (1100, 700), (760, 480), (600, 480)]])
 
-	mask = np.zeros_like(images[0])
-	cv2.fillPoly(mask, src1, (255,255,255)) 
-	mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-	mask = threshIt(mask, 200, 255)
+    mask = np.zeros_like(images[0])
+    cv2.fillPoly(mask, src1, (255,255,255))
+    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    mask = threshIt(mask, 200, 255)
 
 else:
-	for name in sorted(os.listdir(image_dir)):
-	    # print(name)
-	    im = cv2.imread(os.path.join(image_dir, name))
-	    im = cv2.undistort(im, K, D) 
-	    im = cv2.GaussianBlur(im, (5,5), 20.0)
-	    if im is not None:
-	        images.append(im)
-	        image_names.append(name)
-	    else:
-	        print("None")
+    for name in sorted(os.listdir(image_dir)):
+        # print(name)
+        im = cv2.imread(os.path.join(image_dir, name))
+        im = cv2.undistort(im, K, D)
+        im = cv2.GaussianBlur(im, (5,5), 20.0)
+        if im is not None:
+            images.append(im)
+            image_names.append(name)
+        else:
+            print("None")
 
-	src1 = np.array([[(150, 500), (950, 500), (740, 280), (530, 280)]])
+    src1 = np.array([[(150, 500), (950, 500), (740, 280), (530, 280)]])
 
-	mask = np.zeros_like(images[0])
-	cv2.fillPoly(mask, src1, (255,255,255)) 
-	mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-	mask = threshIt(mask, 200, 255)
+    mask = np.zeros_like(images[0])
+    cv2.fillPoly(mask, src1, (255,255,255))
+    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    mask = threshIt(mask, 200, 255)
 
 print(len(images))
 
@@ -187,13 +188,13 @@ for i in range(len(images)):
 
     combined5 = np.zeros_like(img[:,:,0])
     if select_data == 0:
-    	combined5[((white == 1) | ((yellow == 1) | (ubinary == 1))) & (mask == 1)] = 1
+        combined5[((white == 1) | ((yellow == 1) | (ubinary == 1))) & (mask == 1)] = 1
     else:
-    	combined5[((white == 1) | (yellow == 1)) & (mask == 1)] = 1
+        combined5[((white == 1) | (yellow == 1)) & (mask == 1)] = 1
 
-    cv2.imshow("combined1", 255*combined5)
-    if cv2.waitKey(0) & 0xff == 27:
-        cv2.destroyAllWindows()
+    # cv2.imshow("combined1", 255*combined5)
+    # if cv2.waitKey(0) & 0xff == 27:
+    #     cv2.destroyAllWindows()
 
     # combined5 = cv2.warpPerspective(combined5, Hom, (200,400))
 
@@ -201,4 +202,98 @@ for i in range(len(images)):
     # if cv2.waitKey(0) & 0xff == 27:
     #     cv2.destroyAllWindows()
 
+    final_points = []
+    combined5 = np.float32(combined5 * 255)
+    sobelx = cv2.Sobel(combined5, cv2.CV_64F, 1, 0, ksize=5)
+    # dst = cv2.Canny(np.uint8(sobelx), 50, 200, None, 3)
+    # cv2.imshow('edges', dst)
+    # cv2.waitKey(1)
+    lines = cv2.HoughLines(np.uint8(sobelx), 1, np.pi / 100, 100)
+    sobelx = np.float32(sobelx)
+    cdst = cv2.cvtColor(sobelx, cv2.COLOR_GRAY2BGR)
+    xPositionsLeft = []
+    if lines is not None:
+        for j in range(0, len(lines)):
+            rho = lines[j][0][0]
+            theta = lines[j][0][1]
+            a = math.cos(theta)
+            b = math.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+            pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+            # cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
+            xPositionsLeft.append((pt1[0] + pt2[0]) / 2)
+    if lines is not None:
+        # Find the left most line
+        xleft = [xminval for xminval in xPositionsLeft if xminval < 80]
+        # print(len(xleft))
+        leftMost = int((len(xleft) / 2) + 0.5) - 1
+        if leftMost >= 0:
+            leftMost = xPositionsLeft.index(xleft[leftMost])
+            rho = lines[leftMost][0][0]
+            theta = lines[leftMost][0][1]
+            a = math.cos(theta)
+            b = math.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+            pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+            cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
+            final_points.append([pt1, pt2])
+    lines = cv2.HoughLines(np.uint8(sobelx), 1, np.pi / 100, 180)
+    xPositionsRight = []
+    if lines is not None:
+        for j in range(0, len(lines)):
+            rho = lines[j][0][0]
+            theta = lines[j][0][1]
+            a = math.cos(theta)
+            b = math.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+            pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+            # cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
+            xPositionsRight.append((pt1[0] + pt2[0]) / 2)
 
+    if lines is not None:
+        # Find the left most line
+        # leftMost = xPositionsLeft.index(min(xPositionsLeft))
+        xright = [x for x in xPositionsRight if x > 100]
+        rightMost = int((len(xright) / 2) + 0.5) - 1
+        if rightMost >= 0:
+            rightMost = xPositionsRight.index(xright[rightMost])
+            # for j, _ in enumerate(lines[rightMost]):
+            # if (leftMost - rightMost > 5):
+            rho = lines[rightMost][0][0]
+            theta = lines[rightMost][0][1]
+            a = math.cos(theta)
+            b = math.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+            pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+            cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
+            final_points.append([pt1, pt2])
+    # cv2.line(cdst, (80,0), (80,200), (0, 255, 0), 3, cv2.LINE_AA)
+    cv2.imshow('final', cdst)
+    cv2.waitKey(1)
+
+    # hom_inverse = np.linalg.inv(Hom)
+    # new_points = []
+    # if len(final_points) == 2:
+    # 	for num in range(2):
+    # 		new_points.append(np.matmul(hom_inverse, [final_points[num][0][0], final_points[num][0][1], 1]))
+    # 		new_points.append(np.matmul(hom_inverse, [final_points[num][1][0], final_points[num][1][1], 1]))
+    #
+    # 	cv2.circle(img, (int(new_points[0][0] / new_points[0][2]), int(new_points[0][1] / new_points[0][2])), 5,
+    # 			   (255, 0, 0), -1)
+    # 	cv2.circle(img, (int(new_points[1][0] / new_points[1][2]), int(new_points[1][1] / new_points[1][2])), 5,
+    # 			   (255, 0, 0), -1)
+    # 	cv2.circle(img, (int(new_points[2][0] / new_points[2][2]), int(new_points[2][1] / new_points[2][2])), 5,
+    # 			   (255, 0, 0), -1)
+    # 	cv2.circle(img, (int(new_points[3][0] / new_points[3][2]), int(new_points[3][1] / new_points[3][2])), 5,
+    # 			   (255, 0, 0), -1)
+    #
+    # 	cv2.imshow("poly_points", img)
+    # 	cv2.waitKey(1)
